f72cf19f46d2ce673d1382db39927ab6667ca5c4
Ori Lindner <olindner@umich.edu>
2024-01-08 23:39:52 -0700

    @lemonseller solved the problem. Needed more 5tyl3. Now opens and is centered around Broomfield.

--- App.tsx
+++ App.tsx
@@ -1,72 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location'; // Import the Location module
 
-const App = () => {
-  const [userLocation, setUserLocation] = useState<any>(null);
-
-  useEffect(() => {
-    const updateUserLocation = (location: any) => {
-      setUserLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
-    };
-
-    const watchUserLocation = async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status === 'granted') {
-        const location = await Location.getCurrentPositionAsync({});
-        updateUserLocation(location);
-
-        const locationSubscription = await Location.watchPositionAsync(
-          { accuracy: Location.Accuracy.High, timeInterval: 5000 },
-          (newLocation) => {
-            updateUserLocation(newLocation);
-          }
-        );
-
-        return () => {
-          if (locationSubscription) {
-            locationSubscription.remove();
-          }
-        };
-      }
-    };
-
-    watchUserLocation();
-
-    return () => {
-      // Cleanup (if needed) when component unmounts
-    };
-  }, []);
-
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello Expo6!</Text>
-      {/* {typeof document !== 'undefined' && (
-       <MapView
-       style={{ flex: 1, width: '100%' }}
-       initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421,
-       }}
-     >
-       <Marker
-         coordinate={{
-           latitude: 37.78825,
-           longitude: -122.4324,
-         }}
-         title="Marker Title"
-         description="Marker Description"
-       />
-     </MapView>     
-      )} */}
+    <View style={styles.container}>
+      <MapView style={styles.map} 
+      initialRegion={{
+        latitude: 39.9208636,
+        longitude: -105.1162414,
+        latitudeDelta: 0.0122,
+        longitudeDelta: 0.0121,
+      }}  
+      >
+        <Marker
+          coordinate={{
+            latitude: 39.9208636,
+            longitude: -105.1162414,
+          }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
     </View>
-  );
-};
 
-export default App;
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});

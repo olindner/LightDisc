36ac9e7525e05447cfeca91b27042554572c84b2
Ori Lindner <olindner@umich.edu>
2024-01-11 11:33:34 -0700

    Map should now init current phone location
    Have some hardcoded debugging onscreen

--- App.tsx
+++ App.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { LocationObjectCoords } from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObjectCoords | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <MapView style={styles.map} 
-      initialRegion={{
-        latitude: 39.9208636,
-        longitude: -105.1162414,
-        latitudeDelta: 0.0122,
-        longitudeDelta: 0.0121,
-      }}  
-      >
-        <Marker
-          coordinate={{
-            latitude: 39.9208636,
-            longitude: -105.1162414,
+      <Text style ={styles.text}>Lat: {location?.latitude}, long: {location?.longitude}</Text>
+      {errorMsg && <Text>"Error: "{errorMsg}</Text>}
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0122,
+            longitudeDelta: 0.0121,
           }}
-          title="Me"
-          description="You Are Here"
-        />
-      </MapView>
+        >
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="Me"
+            description="You Are Here"
+          />
+        </MapView>
+      )}
     </View>
   );
 }
@@ -33,6 +55,11 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '100%',
+    height: '90%',
+  },
+  text: {
+    flex: 1,
+    paddingTop: 50,
+    textAlign: 'right',
   },
 });
